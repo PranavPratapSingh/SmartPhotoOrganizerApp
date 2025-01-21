6a45d6ecf890be9bf39376c684b3c92b70eb9d95
PranavPratapSingh <pranavpratap1997@gmail.com>
2025-01-21 14:55:09 -0500

    Initial Commit

---
+++ SmartPhotoOrganizerApp/SmartPhotoOrganizerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPhotoOrganizerAppApp.swift
+//  SmartPhotoOrganizerApp
+//
+//  Created by Pranav Pratap Singh on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartPhotoOrganizerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartPhotoOrganizerAppTests/SmartPhotoOrganizerAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  SmartPhotoOrganizerAppTests.swift
+//  SmartPhotoOrganizerAppTests
+//
+//  Created by Pranav Pratap Singh on 1/21/25.
+//
+
+import Testing
+
+struct SmartPhotoOrganizerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
